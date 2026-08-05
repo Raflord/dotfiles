2e4ffb2a15aea9749242a1e3404e6c8b61ebbde6
raflord <rafaelwrobel2000@gmail.com>
2026-08-04 21:24:24 -0300

    build: removing nvim-pack config (unused)

--- nvim-pack/.config/nvim/plugin/01-oil.lua
+++
@@ -1,8 +0,0 @@
-vim.pack.add({ "https://github.com/stevearc/oil.nvim" })
-
-require("oil").setup({
-	columns = { "icon" },
-	keymaps = { ["<C-h>"] = false },
-
-	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory in oil" }),
-})
--- nvim-pack/.config/nvim/plugin/09-mini.lua
+++
@@ -1,6 +0,0 @@
-vim.pack.add({ "https://github.com/echasnovski/mini.nvim" })
-
-require("mini.comment").setup()
-require("mini.surround").setup()
-require("mini.pairs").setup()
-require("mini.icons").setup()
