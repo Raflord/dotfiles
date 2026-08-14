7100697cc1c54212f89f33b53b24082b692545cb
raflord <rafaelwrobel2000@gmail.com>
2026-08-13 21:44:29 -0300

    removed override from coloscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,15 +2,9 @@ return {
 	{
 		"webhooked/kanso.nvim",
 		config = function()
-			local color_palette = require("kanso.colors").setup({ theme = "zen" }).palette
 			require("kanso").setup({
 				background = { dark = "zen", light = "pearl" },
 				foreground = "saturated",
-				overrides = function()
-					return {
-						["@lsp.type.namespace.go"] = { fg = color_palette.green3Saturated },
-					}
-				end,
 			})
 			vim.cmd.colorscheme("kanso")
 		end,
